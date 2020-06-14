ac5122532d690b387956aef578b13c67630dad75
Carl <wennerblad@hotmail.com>
2020-06-14 18:49:08 +0200

    Initial commit

---
+++ Lesson 6 making a website including Java/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson_6_making_a_website_including_Java
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lesson 6 making a website including Java/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lesson_6_making_a_website_including_Java.MvcApplication" Language="C#" %>
